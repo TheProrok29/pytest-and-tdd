522ad148930d2f8263ec74a882502f0ab3615796
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2018-10-19 16:50:44 +0200

    Learning how to mark test: skip, skipif, xfail

--- twitter.py
+++ twitter.py
@@ -33,9 +33,8 @@ class Twitter(object):
 
         url = urljoin(USERS_API, self.username)
 
-        #        import pdb; pdb.set_trace()
-        import wdb;
-        wdb.set_trace()
+        #   import pdb; pdb.set_trace()
+        #   import wdb; wdb.set_trace()
         resp = requests.get(url)
         return resp.json()['avatar_url']
 
